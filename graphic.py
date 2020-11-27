1858a69f1353a599811dcfb04835136ba9f285c8
PaoloMarconi95 <Internet0Git>
2020-11-27 09:57:26 +0100

    Improvement, taxes now supported as *Name*,*value*% in the .csv

--- graphic.py
+++ graphic.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
-def main(expenses, names, final):
-    explode = list()
-    for i in expenses:
-        explode.append(0)
-    explode = tuple(explode)
-    expenses.append(final)
-    names.append("Net")
-    explode = explode + (0.05,)
-    plt.pie(expenses, explode=explode, labels=names, startangle=0)
-    plt.show()
 
+def main(expenses, names, finalNet):
+    # all fraction of the pies must be 0
+    explode = tuple(0 for _ in range(len(expenses)))
+    # Adding the net
+    expenses.append(finalNet)
+    names.append("Net = " + str(finalNet))
+    # The net must explode more than other slices
+    explode = explode + (0.1,)
+    # final plot
+    plt.pie(expenses, explode=explode, labels=names, pctdistance=2, radius=1.3)
+    plt.show()
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from graphic import main as grp
 
 gross_wage = 4000
 
+
 def main():
     expenses, names = csv_to_array("Expenses.csv")
-    final = estimate_final(expenses)
-    taxes = gross_wage*13.7/100
+    finalNet, taxes = estimate_final(expenses)
+    clean_out(expenses, names)
     expenses.append(taxes)
     names.append("Taxes")
-    grp(expenses, names, final)
+    grp(expenses, names, finalNet)
 
 
 def csv_to_array(filename):
@@ -19,18 +20,47 @@ def csv_to_array(filename):
         reader = csv.DictReader(csv_file)
         for line in reader:
             exp = line['Cost']
-            nam = line['Name']
+            name = line['Name']
             expenses.append(exp)
-            names.append(nam)
+            names.append(name)
     return expenses, names
 
 
 def estimate_final(expenses):
+    taxes = []
     total_expenses = 0
     for expense in expenses:
-        total_expenses = total_expenses + int(expense)
-    net_wage = (gross_wage - (gross_wage*13.7/100))
-    return net_wage - total_expenses
+        if is_int(expense):
+            total_expenses = total_expenses + int(expense)
+        else:
+            taxes.append(expense)
+    no_taxes_wage = gross_wage
+    total_taxes = 0
+    if len(taxes) > 0:
+        for tax in taxes:
+            tax_to_apply = tax.replace("%", "")
+            tax_to_apply = float(tax_to_apply)
+            tax_estimated = gross_wage * tax_to_apply / 100
+            total_taxes = total_taxes + tax_estimated
+            no_taxes_wage = no_taxes_wage - tax_estimated
+    finalNet = no_taxes_wage - total_expenses
+    return finalNet, total_taxes
+
+
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+
+def clean_out(expenses, names):
+    for exp in expenses:
+        if "%" in exp:
+            i = expenses.index(exp)
+            names.remove(names[i])
+            expenses.remove(expenses[i])
 
 
 main()
